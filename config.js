0b604f9f98e008074ba417cc0c146df520e8d605
ameerkhadi <ameerhadigithub@gmail.com>
2026-07-27 15:08:01 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // 👉 عدّل هذا فقط: رابط نشر Apps Script (Deploy → New deployment → Web app)
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwjd0xlil8g5PbMFgs_PFVZHU-k6AlGGagnT5gcZTuin8M6TnIf-MqPL6p3dvH9-g30/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwr_sKtWqLJKBJO6wlUY-RczaqAecz1tab6RvqK30M2uGcIZoDjzNlCYDN9KVcLx1Ua9w/exec";
